a40beca3a2294558503596f424f745d42efcab38
debarkamondal <debarkamondal@gmail.com>
2023-01-21 16:58:57 +0530

    Feat: adding pagination

--- react-class/src/components/NewsComponent.js
+++ react-class/src/components/NewsComponent.js
@@ -9,6 +9,7 @@ export class NewsComponent extends Component {
       articles: [],
       loading: false,
       page: 1,
+      totalResults: 0,
     }
   }
 
@@ -16,7 +17,10 @@ export class NewsComponent extends Component {
     let url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=2b2ec47caa5f40899bf142dc2645f5f0";
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({ articles: parsedData.articles })
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults
+    })
   }
 
   handlePrevClick = async () => {
@@ -50,13 +54,14 @@ export class NewsComponent extends Component {
               let urlToImage = element.urlToImage ? element.urlToImage : "https://images.wsj.net/im-706795/social";
               let description = element.description ? element.description.substring(0, 150) + "..." : "Our phones, smartwatches and smart glasses will soon get longer battery life and better visibility in daylight";
               let title = element.title ? element.title.substring(0, 45) + "..." : "New Display Tech Is Coming From Apple, Google, Meta and Others - The Wall Street Journal";
-              
+
               return <div key={url} className="col-md-4">
                 <NewsItem title={title} description={description} imgUrl={urlToImage} newsUrl={url} />
               </div>
             })}
           </div>
           <div className="container justify-content-between d-flex">
+            <p>{}</p>
             <button type="button" disabled={this.state.page <= 1} className="btn btn-primary" onClick={this.handlePrevClick}> &larr; Previous</button>
             <button type="button" className="btn btn-primary" onClick={this.handleNextClick}>Next &rarr;</button>
           </div>
