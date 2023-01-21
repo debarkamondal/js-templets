5f9596b598ae61959dff3b90bb1537b992ac0943
debarkamondal <debarkamondal@gmail.com>
2023-01-21 20:34:08 +0530

    refactor

--- react-class/src/components/NewsItem.js
+++ react-class/src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
 
         let { title, description, imgUrl, newsUrl } = this.props;
         return (
-            <div className="card my-2" style={{ width: "18rem" }}>
+            <div className="card my-2">
                 <img src={imgUrl} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
