6bb04d51033b22e6b61ee1be521be3176b728887
debarkamondal <debarkamondal@gmail.com>
2023-01-21 22:12:38 +0530

    Added react router v6.7

--- react-class/src/App.js
+++ react-class/src/App.js
@@ -3,13 +3,34 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import NewsComponent from './components/NewsComponent';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <NewsComponent pageSize={20} category="general"/>,
+  },
+  {
+    path: "/sports",
+    element: <NewsComponent pageSize={20} category="sports"/>,
+  },
+  {
+    path: "/health",
+    element: <NewsComponent pageSize={20} category="health"/>,
+  }
+]);
 
 export default class App extends Component {
   render() {
     return (
       <>
       <Navbar />
-      <NewsComponent pageSize={20} category="sports"/>
+      <RouterProvider router={router} />
+      <Outlet/>
       </>
     )
   }
