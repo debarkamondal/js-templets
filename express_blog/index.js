4fbdc03e3399a4c3b57bb0c04ec5f35ce2c4632f
debarkamondal <debarkamondal@gmail>
2023-01-26 01:48:47 +0530

    re: improved folder structure

--- express_blog/index.js
+++ express_blog/index.js
@@ -3,19 +3,8 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use('/', require(path.join(__dirname, 'routes/blog.js')))
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
-})
-
-app.get('/hello/:name', (req, res) => {
-  res.send('Hello World!' + req.params.name)
-})
-
-app.get('/json', (req, res)=>{
-    res.json({"name": "harry"})
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- express_blog/routes/blog.js
+++ express_blog/routes/blog.js
@@ -1,4 +1,9 @@
-const express = require ('express')
-const router= require (express.Router)
+const express = require('express')
+const router = express.Router();
+const path= require('path')
 
-router.get('/',)
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../templates/index.html'));
+})
+
+module.exports = router
